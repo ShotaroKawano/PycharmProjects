c845f945308e66067dc51af10e1d67f2993ac1b0
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-12-12 16:39:37 +0900

    Learn: python3 pythonの基本

--- python_programming/lesson.py
+++ python_programming/lesson.py
@@ -1,5 +1,23 @@
-a = 'test'
-b = a
-c = b
+s = 'My name is Mike. Hi Mike.'
+print(s)
 
-print(c)
+is_start = s.startswith('My')
+print(is_start)
+is_start = s.startswith('X')
+print(is_start)
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
+
+
+a = 'a'
+print(f'a is {a}')
+
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
