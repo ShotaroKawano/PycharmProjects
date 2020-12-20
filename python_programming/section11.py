8a372125103ad03bb0bdd0cf109c627aeb09bd6e
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-12-20 23:00:32 +0900

    Learn: python3 コンフィグとロギング1

---
+++ python_programming/section11.py
@@ -0,0 +1,40 @@
+# import configparser
+
+# config = configparser.ConfigParser()
+# config['DEFAULT'] = {
+#     'debug': True
+# }
+# config['web_server'] = {
+#     'host': '127.0.0.1',
+#     'port': 80
+# }
+# config['db_server'] = {
+#     'host': '127.0.0.1',
+#     'port': 3306
+# }
+# with open('config.ini', 'w') as config_file:
+#     config.write(config_file)
+
+# config.read('config.ini')
+# print(config['web_server'])
+# print(config['web_server']['host'])
+
+
+import yaml
+
+with open('config.yaml', 'w') as yaml_file:
+    yaml.dump({
+        'web_server': {
+            'host': '127.0.0.1',
+            'port': 80
+        },
+        'db_server': {
+            'host': '127.0.0.1',
+            'port': 3306
+        }
+    }, yaml_file, default_flow_style=False)
+
+with open('config.yaml', 'r') as yaml_file:
+    data = yaml.load(yaml_file)
+    print(data)
+    print(data['web_server']['host'])
